d3a8135393e2b549efdd3a6d56f1515ecefffc9c
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-15 10:23:45 -0400

    Update __init__.py

--- Talent and Weapon Level-Up Material Drops/src/algorithm/__init__.py
+++ Talent and Weapon Level-Up Material Drops/src/algorithm/__init__.py
@@ -8,4 +8,4 @@ Description: The initialization file for the package algorithm.
 
 from algorithm.phase1_get_data import *
 from algorithm.phase2_print_to_file import *
-from algorithm.phase3_modify_workbook import *
+from algorithm.phase3_print_to_workbook import *
