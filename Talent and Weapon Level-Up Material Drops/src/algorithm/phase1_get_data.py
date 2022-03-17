56ace34bdac007db4a43579a073ca06f08dbbf36
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-16 20:42:10 -0400

    Update phase1_get_data.py

--- Talent and Weapon Level-Up Material Drops/src/algorithm/phase1_get_data.py
+++ Talent and Weapon Level-Up Material Drops/src/algorithm/phase1_get_data.py
@@ -19,27 +19,16 @@ def get_talent_data(workbook):
     :return: The dictionary to store the number of type of talent drops,
              sorted in descending order.
     """
-    # Get the worksheet that contains the list of talent domain drops.
     worksheet = utils.get_worksheet(workbook, "Talent Drop Data")
-    # Initialize a dictionary to store the data from the worksheet.
     talent = {}
-    # For each row in the spreadsheet.
     for i, row in enumerate(worksheet.iter_rows()):
-        # Ignore the first row (the header row).
         if i > 0:
-            # Unpack the row.
             p, b, g = row[0].value, row[1].value, row[2].value
-            # Create a string top represent the drop.
             string = f"({p}/{b}/{g})"
-            # If the drop is in the dictionary.
             if string in talent:
-                # Increment the occurrence for that drop by 1.
                 talent[string] += 1
                 pass
-            # If the drop is not in the dictionary.
             else:
-                # Add it as a new entry to the dictionary with an occurrence
-                # of one.
                 talent[string] = 1
                 pass
             pass
@@ -56,28 +45,17 @@ def get_weapon_data(workbook):
     :return: The dictionary to store the number of type of weapon drops,
              sorted in descending order.
     """
-    # Get the worksheet that contains the list of weapon domain drops.
     worksheet = utils.get_worksheet(workbook, "Weapon Drop Data")
-    # Initialize a dictionary to store the data from the worksheet.
     weapon = {}
-    # For each row in the spreadsheet.
     for i, row in enumerate(worksheet.iter_rows()):
-        # Ignore the first row (the header row).
         if i > 0:
-            # Unpack the row.
             y, p = row[0].value, row[1].value
             b, g = row[2].value, row[3].value
-            # Create a string top represent the drop.
             string = f"({y}/{p}/{b}/{g})"
-            # If the drop is in the dictionary.
             if string in weapon:
-                # Increment the occurrence for that drop by 1.
                 weapon[string] += 1
                 pass
-            # If the drop is not in the dictionary.
             else:
-                # Add it as a new entry to the dictionary with an occurrence
-                # of one.
                 weapon[string] = 1
                 pass
             pass
