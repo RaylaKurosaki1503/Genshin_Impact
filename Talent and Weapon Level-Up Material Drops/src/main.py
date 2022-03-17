d7e077c1fb1cf7fc9ca4504608da4d5817fd2172
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-16 20:42:15 -0400

    Update main.py

--- Talent and Weapon Level-Up Material Drops/src/main.py
+++ Talent and Weapon Level-Up Material Drops/src/main.py
@@ -15,22 +15,15 @@ import algorithm as alg
 
 
 def main():
-    # Hardcode the path from source root.
     path = "../data/talent_weapon_drops.xlsx"
-    # Check if the file exists
     if not file_exists(path):
-        # Exits the program if the file does not exist.
         print("Place your excel file in the data directory.\n"
               "Make sure it is called \"talent_weapon_drops.xlsx\"")
         sys.exit(0)
         pass
-    # Get the Excel Spreadsheet.
     workbook = utils.get_workbook(path)
-    # Get the talent and weapon drops.
     talent, weapon = alg.phase1_main(workbook)
-    # Print the analysis to a text file.
     alg.phase2_main(talent, weapon)
-    # Update the analyzed data in the workbook.
     alg.phase3_main(talent, weapon)
     pass
 
