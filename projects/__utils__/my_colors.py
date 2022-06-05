7859c6053aeb3bf77b15f73b6f0060bb782a3b37
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-06-05 14:47:44 -0400

    Added utility files

---
+++ projects/__utils__/my_colors.py
@@ -0,0 +1,76 @@
+"""
+Author: Rayla Kurosaki
+
+File: my_colors.py
+
+Description: This file returns a list of certain color sets.
+"""
+
+
+def get_math_colors():
+    """
+    Gets the standard mathematics colors.
+    """
+    black = "#000000"
+    red = "#FF0000"
+    blue = "#0000FF"
+    return [black, red, blue]
+
+
+def get_shades_cyan():
+    """
+    Gets the different shades of cyan.
+    """
+    aqua = "#00FFFF"
+    celeste = "#B2FFFF"
+    electric_blue = "#7DF9FF"
+    sky_blue = "#80DAEB"
+    turquoise = "#40E0D0"
+    return [aqua, celeste, electric_blue, sky_blue, turquoise]
+
+
+def get_shades_pink():
+    """
+    Gets the different shades of pink.
+    """
+    pink = "#FFC0CB"
+    hot_pink = "#FF69B4"
+    deep_pink = "#FF1493"
+    pink_lace = "#FFDDF4"
+    cherry_blossom_pink = "#FFB7C5"
+    light_hot_pink = "#FFB3DE"
+    lavender_pink = "#FBAED2"
+    rose_pink = "#FF66CC"
+    light_deep_pink = "#FF5CCD"
+    ultra_pink = "#FF6FFF"
+    shocking_pink = "#FC0FC0"
+    steel_pink = "#CC33CC"
+    return [pink, hot_pink, deep_pink, pink_lace, cherry_blossom_pink,
+            light_hot_pink, lavender_pink, rose_pink, light_deep_pink,
+            ultra_pink, shocking_pink, steel_pink]
+
+
+def get_genshin_colors(color_type):
+    """
+    Gets specific color genshin impact sets.
+    """
+    match color_type:
+        case "Rarity":
+            return {5: "#A1662A", 4: "#64578D"}
+        case "Elements":
+            return {"Anemo": "#A4F6CC", "Geo": "#F1D85F",
+                    "Electro": "#DABBFD", "Dendro": "#A7D43B",
+                    "Hydro": "#06E9F9", "Pyro": "#FBAB71", "Cryo": "#A6FDFE"}
+        case "Nation":
+            return {"Mondstadt": "#A4F6CC", "Liyue": "#F1D85F",
+                    "Inazuma": "#DABBFD", "Sumeru": "#A7D43B",
+                    "Fontaine": "#06E9F9", "Natlan": "#FBAB71",
+                    "Snezhnaya": "#A6FDFE"}
+    return None
+
+
+def get_red_yellow_green_colorscale():
+    """
+    Gets a set of colors to define a red-yellow-green colorscale.
+    """
+    return ["#F8696B", "#FFEB84", "#63BE7B"]
