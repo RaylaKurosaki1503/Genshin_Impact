d3c609930c58140c6c694e3d4e42e4a8cf0e9686
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-06-05 14:47:34 -0400

    Create __init__.py

---
+++ projects/Talent_Weapon_Material_Drop_Analysis/algorithm/__init__.py
@@ -0,0 +1,11 @@
+"""
+Author: Rayla Kurosaki
+
+File: __init__.py
+
+Description: The initialization file for the package algorithm.
+"""
+
+from projects.Talent_Weapon_Material_Drop_Analysis.algorithm.phase1_get_data import *
+from projects.Talent_Weapon_Material_Drop_Analysis.algorithm.phase2_print_to_file import *
+from projects.Talent_Weapon_Material_Drop_Analysis.algorithm.phase3_print_to_workbook import *
